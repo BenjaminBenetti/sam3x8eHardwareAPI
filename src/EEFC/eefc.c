a7136af13ff8bcc67a31c84873a9c9d78366d815
Benjamin Benetti <ben.benetti@shaw.ca>
2018-04-07 22:30:37 -0700

    EEFC interface work + hide rawEEFCCommand interface form external code. + finish read flash descriptor + add a new struct FlashDescriptor which is intended to be used with above.   said structure ofc describes the flash layout + add function for getting the base address of the flash regions controlled by the   given flash controller

--- src/EEFC/eefc.c
+++ src/EEFC/eefc.c
@@ -3,18 +3,41 @@
 #include <string.h>
 #include <stdbool.h>
 
-int rawEEFCCommand(uint8_t controller, uint8_t cmd, uint16_t farg, void * resBuff){
-  Efc * efcPtr = NULL;
-  switch(controller){
+//EEFC command id list
+#define EEFC_GETD 0x00 // get descriptor
+#define EEFC_WP   0x01 // write page
+#define EEFC_WPL  0x02 // write page & lock
+#define EEFC_EWP  0x03 // erase page & write
+#define EEFC_EWPL 0x04 // erase page & write & lock
+#define EEFC_EA   0x05 // danger Batman! (will erase all flash!)
+#define EEFC_SLB  0x08 // lock page
+#define EEFC_CLB  0x09 // unlock page
+#define EEFC_GLB  0x0A // check if page locked
+#define EEFC_SGPB 0x0B
+#define EEFC_CGPB 0x0C
+#define EEFC_GGPB 0x0D
+#define EEFC_STUI 0x0E
+#define EEFC_SPUI 0x0F
+#define EEFC_GCALB 0x10
+
+//return pointer to EEFC chip based on con
+Efc * getEEFC(uint16_t con){
+  //only two chips on sam3x8e
+  Efc * Eptr = NULL;
+  switch(con){
     case 0:
-      efcPtr = EFC0;
+      Eptr = EFC0;
       break;
     case 1:
-      efcPtr = EFC1;
+      Eptr = EFC1;
       break;
-    default:
-      return false;
   }
+  return Eptr;
+}
+
+
+static int rawEEFCCommand(uint8_t controller, uint8_t cmd, uint16_t farg){
+  Efc * efcPtr = getEEFC(controller);
 
   //execute command
   efcPtr->EEFC_FCR = EEFC_FCR_FKEY(0x5A) | EEFC_FCR_FARG(farg) | EEFC_FCR_FCMD(cmd);
@@ -31,5 +54,39 @@ int rawEEFCCommand(uint8_t controller, uint8_t cmd, uint16_t farg, void * resBuf
 }
 
 
-void readFlashDescriptor(void * buffer){
+void readFlashDescriptor(struct FlashDescriptor * fDesc, uint16_t controller, uint32_t lenPlanesMax, uint32_t lenLockRegionMax){
+  Efc * efcPtr = getEEFC(controller);
+
+  rawEEFCCommand(controller,EEFC_GETD, 0x0);
+
+  fDesc->flashID        = efcPtr->EEFC_FRR;
+  fDesc->flashSize      = efcPtr->EEFC_FRR;
+  fDesc->pageSize       = efcPtr->EEFC_FRR;
+  fDesc->numberOfPlanes = efcPtr->EEFC_FRR;
+
+  int i = 0;
+  while(i < fDesc->numberOfPlanes && i < lenPlanesMax){
+    *(fDesc->bytesInPlane + i)  = efcPtr->EEFC_FRR;
+    i++;
+  }
+  fDesc->lenBytesInPlane = i;
+
+  fDesc->numberOfLockBits = efcPtr->EEFC_FRR;
+
+  i = 0;
+  while(i < fDesc->numberOfLockBits && i < lenLockRegionMax){
+    *(fDesc->bytesPerLockRegion + i) = efcPtr->EEFC_FRR;
+    i++;
+  }
+  fDesc->lenBytesPerLockRegion = i;
+}
+
+void * getFlashStartAddress(uint16_t controller){
+  //return flash address as described on page 31 of sam3x8e datasheet
+  switch(controller){
+    case 0:
+      return (void *) 0x00080000;
+    case 1:
+      return (void *) 0x000C0000;
+  }
 }
--- src/EEFC/eefc.h
+++ src/EEFC/eefc.h
@@ -1,50 +1,56 @@
 /**
 @file
-@brief EEFC (enhanced flash controller) hardware interface 
+@brief EEFC (enhanced flash controller) hardware interface
 */
 
 #ifndef EEFC_H_
 #define EEFC_H_
 #include <stdint.h>
 
-//EEFC command id list
-#define EEFC_GETD 0x00 // get descriptor
-#define EEFC_WP   0x01 // write page
-#define EEFC_WPL  0x02 // write page & lock
-#define EEFC_EWP  0x03 // erase page & write
-#define EEFC_EWPL 0x04 // erase page & write & lock
-#define EEFC_EA   0x05 // danger Batman! (will erase all flash!)
-#define EEFC_SLB  0x08 // lock page
-#define EEFC_CLB  0x09 // unlock page
-#define EEFC_GLB  0x0A // check if page locked
-#define EEFC_SGPB 0x0B
-#define EEFC_CGPB 0x0C
-#define EEFC_GGPB 0x0D
-#define EEFC_STUI 0x0E
-#define EEFC_SPUI 0x0F
-#define EEFC_GCALB 0x10
-//end EEFC command id list
+/**
+  this structure describes the layout of flash memory on the board.
+*/
+struct FlashDescriptor{
+  uint32_t flashID;
+  uint32_t flashSize;
+  uint32_t pageSize;
+  uint32_t numberOfPlanes;
+
+  uint32_t * bytesInPlane;//list
+  uint32_t lenBytesInPlane;
+
+  uint32_t numberOfLockBits;
+
+  uint32_t * bytesPerLockRegion; // lsit
+  uint32_t lenBytesPerLockRegion;
+
+} FlashDescriptor;
 
 /**
-  execute a raw EEFC command. see EEFC section in sam3x8e manual for command list.
-  @param controller
-  id of the EEFC that the command is to be run on.
-  Sam3x8e has two. 0 and 1 respectivly
-  @param cmd
-  EEFC command to execute
-  @param farg
-  argument for the command
-  @param resBuff
-  if the command does produce a result, it will be stored here, with NO BOUNDS CHECK!
-  @return true on success, false on failure
+  the number of flash controllers on the sam3x8e
 */
-int rawEEFCCommand(uint8_t controller,uint8_t cmd, uint16_t farg, void * resBuff);
+#define FLASH_CONTROLLER_COUNT 2
 
 /**
-  read the flash descriptor form the EEFC controller.
-  BUFFER IS TMP TMP TMP !!! TODO FIX
+  read the flash descriptor form the EEFC controller. and populate the flash descriptor structure
+  @param fDesc the flash descriptor strcture to be populated by the function call.
+  Note, you must have pre allocated, bytesInPlane and bytesPerLockRegion arrays
+  @param controller the controller on which you want to read the flash descriptor
+  @param lenPlanesMax the max number of plane words that can be placed in bytesInPlane array.
+  @param lenLockRegionMax the max number of lock region words that can be placed in bytesPerLockRegion
+  @return the flash descriptor structure will be filled out. lenBytesInPlane and lenBytesPerLockRegion
+  will be set to the number of actual values in there respective arrays. if lenBytesInPlane or lenBytesPerLockRegion is
+  equal to there max counterparts, there may be additional regions / planes on the flash chip.
+  @see FlashDescriptor
+  @see FLASH_CONTROLLER_COUNT
 */
-void readFlashDescriptor(void * buffer);
+void readFlashDescriptor(struct FlashDescriptor * fDesc, uint16_t controller, uint32_t lenPlanesMax, uint32_t lenLockRegionMax);
 
+/**
+  get the start address for controller
+  @param controller the controller for which the start address is to be fetech.
+  @return the address at which said flash memory starts 
+*/
+void * getFlashStartAddress(uint16_t controller);
 
 #endif /*EEFC_H_*/
