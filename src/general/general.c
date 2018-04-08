18e16c649d1ce7e0b151db5f746e38d009356544
Benjamin Benetti <ben.benetti@shaw.ca>
2018-04-07 21:19:31 -0700

    new general section + added system clock setup to general section (moved out of kernel)

---
+++ src/general/general.c
@@ -0,0 +1,40 @@
+#include "general.h"
+#include <sam3xa/include/sam3x8e.h>
+
+void setupMainClock(void){
+  // ???? Mhz
+  EFC0->EEFC_FMR = EEFC_FMR_FWS(4);
+  EFC1->EEFC_FMR = EEFC_FMR_FWS(4);
+
+  PMC->CKGR_MOR = CKGR_MOR_MOSCRCEN | CKGR_MOR_MOSCXTEN | CKGR_MOR_KEY(0x37) | CKGR_MOR_MOSCXTST(0x8u);
+
+  //wait for clock to start
+  while(!(PMC->PMC_SR & PMC_SR_MOSCXTS));
+
+  //select clock
+  PMC->CKGR_MOR = CKGR_MOR_MOSCRCEN | CKGR_MOR_MOSCXTEN | CKGR_MOR_MOSCSEL | CKGR_MOR_KEY(0x37) | CKGR_MOR_MOSCXTST(0x8u);
+
+  //wait for select
+  while(!(PMC->PMC_SR & PMC_SR_MOSCSELS));
+
+  PMC->PMC_MCKR = (PMC->PMC_MCKR & ~(uint32_t)PMC_MCKR_CSS_Msk) | PMC_MCKR_CSS_MAIN_CLK;
+
+  while(!(PMC->PMC_SR & PMC_SR_MCKRDY));
+
+
+  PMC->CKGR_PLLAR = CKGR_PLLAR_ONE | CKGR_PLLAR_MULA(0xdUL) | CKGR_PLLAR_DIVA(0x1UL) | CKGR_PLLAR_PLLACOUNT(0x3fU);
+
+  //wait for lock
+  while(!(PMC->PMC_SR & PMC_SR_LOCKA));
+
+
+  PMC->PMC_MCKR = PMC_MCKR_PRES_CLK_2 | PMC_MCKR_CSS_MAIN_CLK;
+
+  while(!(PMC->PMC_SR & PMC_SR_MCKRDY));
+
+  PMC->PMC_MCKR = PMC_MCKR_PRES_CLK_2 | PMC_MCKR_CSS_PLLA_CLK;
+
+  while(!(PMC->PMC_SR & PMC_SR_MCKRDY));
+
+  //we now should now be at 84 Mhz
+}
---
+++ src/general/general.h
@@ -0,0 +1,17 @@
+/**
+@file
+@brief general / mics hardware functions
+*/
+#ifndef GENERAL_HW_H_
+#define GENERAL_HW_H_
+
+/**
+  initialize the sam3x8e main clock. this clock drives the entire system!
+  i.e. all peripherals and the main core clock. many features may not work
+  correctly if the main clock is not running at 84Mhz. call this near the start
+  of the program.
+*/
+void setupMainClock(void);
+
+
+#endif
