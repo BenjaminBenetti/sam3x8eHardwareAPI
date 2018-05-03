87a0aaf89272b21b50d412bda624f20e88a2897a
Benjamin Benetti <ben.benetti@shaw.ca>
2018-05-03 14:04:06 -0700

    PIO functionality + digital io functions. output / input, set pin / read pin. that sort of stuff. + see pio.h for full function list

---
+++ src/PIO/pio.c
@@ -0,0 +1,114 @@
+#include "pio.h"
+
+
+#define REG_PIO_PER 0
+#define REG_PIO_PDR 1
+#define REG_PIO_PSR 2
+#define REG_PIO_OER 4
+#define REG_PIO_ODR 5
+#define REG_PIO_OSR 6
+//...
+#define REG_PIO_SODR 12
+#define REG_PIO_CODR 13
+//...
+#define REG_PIO_PDSR 15
+//...
+#define REG_PIO_PUDR 24
+#define REG_PIO_PUER 25
+//...
+#define REG_PIO_OWER 40
+#define REG_PIO_OWDR 41
+static uint32_t * lookupRegister(uint8_t bank, uint8_t reg){
+  switch(bank){
+    case PIO_BANK_A:
+      return (uint32_t *) (&REG_PIOA_PER + reg);
+
+    case PIO_BANK_B:
+      return (uint32_t *) (&REG_PIOB_PER + reg);
+
+    case PIO_BANK_C:
+      return (uint32_t *) (&REG_PIOC_PER + reg);
+
+    case PIO_BANK_D:
+      return (uint32_t *) (&REG_PIOD_PER + reg);
+
+    default:
+      return 0;
+  }
+}
+
+
+void enableIOBank(uint8_t bank){
+  if(bank >= PIO_BANK_A && bank <= PIO_BANK_D){
+    REG_PMC_PCER0 |= (1 << bank);
+  }
+}
+
+void enableAllIOBanks(){
+  enableIOBank(PIO_BANK_A);
+  enableIOBank(PIO_BANK_B);
+  enableIOBank(PIO_BANK_C);
+  enableIOBank(PIO_BANK_D);
+}
+
+
+void disableIOBank(uint8_t bank){
+  if(bank >= PIO_BANK_A && bank <= PIO_BANK_D){
+    REG_PMC_PCER0 &= ~(1 << bank);
+  }
+}
+
+
+void enablePin(uint8_t bank, uint32_t pin, uint8_t direction){
+  uint32_t * enReg = lookupRegister(bank, REG_PIO_PER);
+
+  *enReg |= pin;
+  setPinDirection(bank,pin,direction);
+}
+
+void disablePin(uint8_t bank, uint32_t pin){
+  uint32_t * dReg = lookupRegister(bank, REG_PIO_PDR);
+  *dReg |= pin;
+}
+
+void setPinDirection(uint8_t bank, uint32_t pin, uint8_t direction){
+  if(direction == PIO_IN){
+    uint32_t * outputDisableReg = lookupRegister(bank, REG_PIO_ODR);
+    uint32_t * outputWriteDisReg = lookupRegister(bank, REG_PIO_OWDR);
+    *outputDisableReg |= pin;
+    *outputWriteDisReg |= pin;
+  }
+  else {
+    uint32_t * outputEnableReg = lookupRegister(bank, REG_PIO_OER);
+    uint32_t * outputWriteReg = lookupRegister(bank, REG_PIO_OWER);
+    *outputEnableReg |= pin;
+    *outputWriteReg |= pin;
+  }
+}
+
+uint8_t readPin(uint8_t bank, uint32_t pin){
+  uint32_t * pinStatus = lookupRegister(bank,REG_PIO_PDSR);
+  return *(pinStatus) & pin ? 1 : 0;
+}
+
+void setPin(uint8_t bank, uint32_t pin, uint8_t llvl){
+  if(llvl){
+    uint32_t * stReg = lookupRegister(bank,REG_PIO_SODR);
+    *stReg |= pin;
+  }
+  else{
+    uint32_t * clrReg = lookupRegister(bank,REG_PIO_CODR);
+    *clrReg |= pin;
+  }
+}
+
+void setPullup(uint8_t bank, uint32_t pin, bool en){
+  uint32_t * reg;
+  if(en){
+    reg = lookupRegister(bank,REG_PIO_PUER);
+  }
+  else {
+    reg = lookupRegister(bank,REG_PIO_PUDR);
+  }
+  *reg |= pin;
+}
---
+++ src/PIO/pio.h
@@ -0,0 +1,78 @@
+#ifndef D_IO_H_
+#define D_IO_H_
+#include <stdint.h>
+#include <stdbool.h>
+#include <sam3xa/include/sam3x8e.h>
+
+/********************
+ ---- PIO banks ----*/
+#define PIO_BANK_A 11
+#define PIO_BANK_B 12
+#define PIO_BANK_C 13
+#define PIO_BANK_D 14
+/********************/
+
+// -- PIO direction --
+#define PIO_IN 0
+#define PIO_OUT 1
+
+
+/**
+  enable the specified pio bank. see pinout of board for settings
+*/
+void enableIOBank(uint8_t bank);
+
+/**
+  @see enableIOBank
+*/
+void enableAllIOBanks();
+
+/**
+  disable specified pio bank
+*/
+void disableIOBank(uint8_t bank);
+
+
+/**
+  enable the specified pin. i.e. set it to digital output (disable peripheral control)
+  @param io bank the pin belongs to.
+  @param pin the pin to enable. names are like "PIO_PB27" (pin B 27).
+  @param direction the direction of the pin (PIO_IN or PIO_OUT)
+*/
+void enablePin(uint8_t bank, uint32_t pin, uint8_t direction);
+
+/**
+  disable pin.
+  @see enablePin
+*/
+void disablePin(uint8_t bank, uint32_t pin);
+
+/**
+  set direction of pin.
+  @param bank the bank that the pin belongs to. A,B,C,D
+  @param pin the pin to modify
+  @param direction new direction setting for the pin IN/OUT
+*/
+void setPinDirection(uint8_t bank, uint32_t pin, uint8_t direction);
+
+/**
+  read signal on pin.
+  @retrun 0 or 1
+*/
+uint8_t readPin(uint8_t bank, uint32_t pin);
+
+/**
+  set logic level on pin
+  @param bank io bank
+  @param pin pin to set
+  @param llvl logic level to set on pin. 1 or 0
+*/
+void setPin(uint8_t bank, uint32_t pin, uint8_t llvl);
+
+/**
+  enable or disable the pullup on the given pin 
+*/
+void setPullup(uint8_t bank, uint32_t pin, bool en);
+
+
+#endif /*D_IO_H_*/
